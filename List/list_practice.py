7fc33886d96c1b1d852772863f72382c458a549d
kikocu <49646222+kikocu@users.noreply.github.com>
2025-10-13 21:02:51 +0000

    exercice 1

--- List/list_practice.py
+++ List/list_practice.py
@@ -1,1 +1,3 @@
-
+days=[ "Monday", "Tuesday", "Wednesday", "Wednesday", "Friday",  "Saturday"]   # This is a list of string
+days[3]="Thursday"
+print(days)
---
+++ test.py
@@ -0,0 +1,7 @@
+
+numbers=[10,11,25,37,42,51,69,70]   # this is an example of list 
+print(numbers)
+
+print(numbers[0:2])
+
+print(numbers[2:6])
