ffbe9b5d4fe077431a564b7f87dbd03533e7e82a
kikocu <49646222+kikocu@users.noreply.github.com>
2025-10-18 16:05:17 +0000

    let see

--- For_Loop/forloop_practice.py
+++ For_Loop/forloop_practice.py
@@ -1,7 +1,7 @@
 costs=[5,10,3,4]  #this is the coost of each item
 items=[0,1,0,1]  #coeficient (if the item is in the container
 value=0
-for i in range(len(costs)):
+for i in range(len(items)):
     value += costs[i] * items[i]
 
 print("for this example the total value of the container is : ", value, "$")
